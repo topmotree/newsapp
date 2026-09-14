e751621f6becaa14ad21133676c43cb308d42767
topmotree <64073182+topmotree@users.noreply.github.com>
2026-09-14 13:20:22 +0200

    feat: enhance search input field with single line limit and keyboard options

--- app/src/main/java/dev/martinsv/newsapp/news/presentation/list/components/ArticleListTopBar.kt
+++ app/src/main/java/dev/martinsv/newsapp/news/presentation/list/components/ArticleListTopBar.kt
@@ -5,6 +5,8 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.text.input.TextFieldLineLimits
 import androidx.compose.foundation.text.input.TextFieldState
 import androidx.compose.foundation.text.input.clearText
 import androidx.compose.foundation.text.input.rememberTextFieldState
@@ -19,6 +21,7 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import dev.martinsv.newsapp.R
@@ -66,7 +69,6 @@ fun ArticleListTopBar(
         )
 
         AnimatedVisibility(isSearchBarVisible) {
-            //TODO set max lines to 1
             OutlinedTextField(
                 state = searchFieldState,
                 placeholder = {
@@ -89,6 +91,8 @@ fun ArticleListTopBar(
                     }
                 },
                 shape = RoundedCornerShape(32.dp),
+                lineLimits = TextFieldLineLimits.SingleLine,
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text),
                 modifier = Modifier
                     .fillMaxWidth()
                     .padding(horizontal = 16.dp)
