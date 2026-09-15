6d8e35a0f3021245a10674e4e801a49e1fda9810
topmotree <64073182+topmotree@users.noreply.github.com>
2026-09-15 15:50:00 +0200

    test: add unit tests for ArticleListViewModel

--- app/src/main/java/dev/martinsv/newsapp/news/data/mapper/ArticleMapper.kt
+++ app/src/main/java/dev/martinsv/newsapp/news/data/mapper/ArticleMapper.kt
@@ -17,6 +17,7 @@ class ArticleMapper @Inject constructor() {
             description = dto.description,
             url = dto.url,
             urlToImage = dto.urlToImage,
+            //TODO test it
             publishedAt = dto.publishedAt?.let { Instant.parse(it) },
             content = dto.content
         )
