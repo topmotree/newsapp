08e804086bee37c3e352ad1dafbd212b5c48daea
topmotree <64073182+topmotree@users.noreply.github.com>
2026-09-15 20:08:51 +0200

    refactor: remove ArticleListUiState interface

--- app/src/main/java/dev/martinsv/newsapp/news/presentation/model/ArticleFetchUiState.kt
+++
@@ -1,7 +0,0 @@
-package dev.martinsv.newsapp.news.presentation.model
-
-sealed interface ArticleListUiState {
-    object Loading : ArticleListUiState
-    data class Success(val articles: List<ArticleUiModel>) : ArticleListUiState
-    data object Error : ArticleListUiState
-}
